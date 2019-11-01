b4373ffa9f653e857cd283678a5648b4e6548827
Rehan <rehan.siddiqui98@yahoo.com>
2019-11-01 17:23:00 +0500

    Initial Commit

---
+++ WindowsFormsApp/TestForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
